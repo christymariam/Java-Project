dcb6feef0818bf67e9f4552199be89687977872e
Christy Mariam <christymariamabraham2028@cs.sjcetpalai.ac.in>
2025-10-27 18:39:26 +0530

    Create userdatab.php

---
+++ userdatab.php
@@ -0,0 +1,10 @@
+CREATE DATABASE userdatab;
+
+USE userdb;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
